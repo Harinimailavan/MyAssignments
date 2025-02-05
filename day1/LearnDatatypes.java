18644c5f76a1136763ad1e1c3446ba5d6f46faec
Harinimailavan <harinimailforother@gmail.com>
2025-02-05 18:12:19 +0530

    Add files via upload

---
+++ day1/LearnDatatypes.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte ageOfPerson=31;
+		short depositAmount=30000;
+		int deposit=50000;
+		long mobileNumber=8526511170l;
+		float rateOfInterest=9.3F;
+		double amount2=0.011555244;
+		System.out.println(amount2);
+		float amount1=0.0115552441267f;
+		System.out.println(amount1);
+		char logoOfTestleaf='T';
+		char asciivalue =65;
+				System.out.println(logoOfTestleaf);
+				System.out.println(asciivalue);
+		boolean doYouKnowJava=true;
+		System.out.println(doYouKnowJava);
+		String name="Jack";
+		
+	}
+
+}
